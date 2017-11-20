32030102ca06b916fd2a2f8207fe08ff8e4990ed
nikrb <nik_rb@yahoo.com>
2017-11-20 00:18:59 +0000

    copy tutorial code across

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { Provider} from 'react-redux';
+import { createStore} from 'redux';
+import todoApp from './reducers';
+// import './index.css';
+import App from './components/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+let store = createStore( todoApp);
+
+ReactDOM.render(
+  <Provider store={store} >
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
