e97f6fbe16eeb813d4c6d75e5fa2438cee02bc78
nikrb <nik_rb@yahoo.com>
2017-11-20 00:18:16 +0000

    move App.js to components dir and copy tutorial code

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+// import './App.css';
+import Footer from './Footer';
+import AddTodo from '../containers/AddTodo';
+import VisibleTodoList from '../containers/VisibleTodoList';
+
+const App = () => (
+  <div>
+    <AddTodo />
+    <VisibleTodoList />
+    <Footer />
+  </div>
+);
+
+export default App;
