f1f05b85c71d803dd00b390ff1df011c8679703a
nikrb <nik_rb@yahoo.com>
2017-11-23 21:46:50 +0000

    comment react-redux connect without parameters

--- src/containers/AddTodo.js
+++ src/containers/AddTodo.js
@@ -22,6 +22,10 @@ let AddTodo = ({ dispatch}) => {
   );
 };
 
+// connect missing params (first and second)
+// first param: no subscribe to store changes
+// second param: dispatch added to component props
+// https://github.com/reactjs/react-redux/blob/master/docs/api.md
 AddTodo = connect()(AddTodo);
 
 export default AddTodo;
